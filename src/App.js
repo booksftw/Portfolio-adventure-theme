a32ab9f4c2ff454bb2bf0aa19bf4950ebc028671
LycanWing <znick46@DESKTOP-PIGFLT3.localdomain>
2021-07-10 22:13:32 -0700

    Buttons in footer all working

--- src/App.js
+++ src/App.js
@@ -10,29 +10,30 @@ import FireCTA from "./components/FireCTA";
 function App() {
   return (
     <div className="nz_root_container">
-      {/* Welcome Container */}
+      {/* Start of first section */}
       <div className="first_section">
         <Welcome />
-
         <div className="animatorIcon_container">
           <AnimatorIcon />
         </div>
       </div>
       {/* End of First Section */}
-
+      {/* Start of third section */}
       <div className="third_section">
         <AboutNick />
         <Timeline />
       </div>
       {/* End of third section */}
-
+      {/* Start of second section */}
       <div className="second_section">
         <FireCTA />
       </div>
-
+      {/* End of second section */}
+      {/* Start of footer */}
       <div className="footer">
         <Footer />
       </div>
+      {/* End of Footer */}
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
 
 const Footer = () => {
+  const [showEmailModal, setShowEmailModal] = useState(false);
+
+  const onEmailClickHandler = () => {
+    console.log("email click handler - show or hide modal");
+    setShowEmailModal(!showEmailModal);
+  };
+
   return (
     <>
+      {/* <!-- The Modal --> */}
+      <div
+        id="myModal"
+        class={
+          "modal " +
+          (showEmailModal ? "footer-email-show" : "footer-email-hidden")
+        }
+      >
+        {/* <!-- Modal content --> */}
+        <div class="modal-content">
+          <span onClick={onEmailClickHandler} class="close">
+            &times;
+          </span>
+          <div className="modal-content-email-container">
+            <p className="modal-email-p">nzachary46@gmail.com</p>
+            <div class="main">
+              <div class="sub-main">
+                <button
+                  class="button-one"
+                  onClick={() => {
+                    navigator.clipboard.writeText("nzachary46@gmail.com");
+                  }}
+                >
+                  Copy Email to Clipboard
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="footer-container">
         <ul>
-          <li>
+          <li onClick={onEmailClickHandler}>
             Email
             <span></span>
             <span></span>
             <span></span>
             <span></span>
           </li>
-          <li>
-            YouTube
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </li>
-          <li>
-            Resume
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </li>
+          <a
+            className="footer-link"
+            href="https://www.youtube.com/channel/UCtzQTLivB84hXDR22gQWQJA"
+          >
+            <li>
+              YouTube
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </li>
+          </a>
+          <a
+            href="https://resume.creddle.io/resume/6qd2rps47cc"
+            className="footer-link"
+          >
+            <li>
+              Resume
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </li>
+          </a>
         </ul>
       </div>
     </>
